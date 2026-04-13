106ddda3212eebbf45d905f77879f35c25cddb59
mushroomfire <934313174@qq.com>
2026-04-13 22:18:49 +0300

    update

--- example/run_train.py
+++ example/run_train.py
@@ -6,20 +6,20 @@ Multi-GPU :  torchrun --nproc_per_node=N run_train.py
 
 from torchnep import train_nep
 
-# train_nep(
-#     config_file="nep.in",
-#     data_file="train.xyz",
-#     output_dir="output_cuda",
-#     device="cuda",
-#     pytorch_only=False,
-#     print_interval=1,
-#     use_compile=True
-# )
+train_nep(
+    config_file="nep_Si.in",
+    data_file="train_Si.xyz",
+    output_dir="output_cuda_Si",
+    device="cuda",
+    pytorch_only=False,
+    print_interval=1,
+    use_compile=True
+)
 
 # train_nep(
-#     config_file="nep.in",
-#     data_file="train.xyz",
-#     output_dir="output_analytical",
+#     config_file="nep_Si.in",
+#     data_file="train_Si.xyz",
+#     output_dir="output_analytical_Si",
 #     device="cuda",
 #     pytorch_only=True,
 #     use_autograd_forces=False,
@@ -27,13 +27,13 @@ from torchnep import train_nep
 #     use_compile=True
 # )
 
-train_nep(
-    config_file="nep.in",
-    data_file="train.xyz",
-    output_dir="output_autograd",
-    device="cuda",
-    pytorch_only=True,
-    use_autograd_forces=True,
-    print_interval=1,
-    use_compile=True
-)
+# train_nep(
+#     config_file="nep.in",
+#     data_file="train.xyz",
+#     output_dir="output_autograd",
+#     device="cuda",
+#     pytorch_only=True,
+#     use_autograd_forces=True,
+#     print_interval=1,
+#     use_compile=True
+# )
