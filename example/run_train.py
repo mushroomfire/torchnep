fc498ffb0e2664c6c20a4eaf313fde0094fcc2e7
mushroomfire <934313174@qq.com>
2026-04-13 22:31:56 +0300

    update

--- example/run_train.py
+++ example/run_train.py
@@ -6,26 +6,26 @@ Multi-GPU :  torchrun --nproc_per_node=N run_train.py
 
 from torchnep import train_nep
 
-train_nep(
-    config_file="nep_Si.in",
-    data_file="train_Si.xyz",
-    output_dir="output_cuda_Si",
-    device="cuda",
-    pytorch_only=False,
-    print_interval=1,
-    use_compile=True
-)
-
 # train_nep(
-#     config_file="nep_Si.in",
-#     data_file="train_Si.xyz",
-#     output_dir="output_analytical_Si",
+#     config_file="nep_AlO.in",
+#     data_file="train_AlO.xyz",
+#     output_dir="output_cuda_AlO",
 #     device="cuda",
-#     pytorch_only=True,
-#     use_autograd_forces=False,
+#     pytorch_only=False,
 #     print_interval=1,
 #     use_compile=True
 # )
+
+train_nep(
+    config_file="nep_AlO.in",
+    data_file="train_AlO.xyz",
+    output_dir="output_analytical_AlO",
+    device="cuda",
+    pytorch_only=True,
+    use_autograd_forces=False,
+    print_interval=1,
+    use_compile=True
+)
 
 # train_nep(
 #     config_file="nep.in",
